bf80b89914df238ef0b373792b659ec085b0d99a
Gabriel Alcequiez <gxbbab@gmail.com>
2026-06-08 08:19:07 -0400

    delete old files that pointed to net10, clean and rebuild

---
+++ eVote360_Pro.Core.Domain/Entities/ElectedOffice.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eVote360_Pro.Core.Domain.Entities
+{
+    public class ElectedOffice
+    {
+        
+    }
+}
--- eVote360_Pro.Infrastructure/obj/Debug/net9.0/eVote360_Pro.Infrastructure.AssemblyInfo.cs
+++ eVote360_Pro.Infrastructure/obj/Debug/net9.0/eVote360_Pro.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eVote360_Pro.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+795ed4019e9922900a104b86d49a62b0fcb94fd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c639c16bc4dc50cf3b57fdaea6ed8bd22bbc3357")]
 [assembly: System.Reflection.AssemblyProductAttribute("eVote360_Pro.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eVote360_Pro.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
