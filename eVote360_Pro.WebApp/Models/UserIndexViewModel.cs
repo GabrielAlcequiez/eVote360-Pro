7e9413783120ee991cfe4327e7a643b29ed7b201
jrc03 <joserdlc2727@gmail.com>
2026-06-10 20:19:05 -0400

    feat: implement UserController for admin user CRUD operations

---
+++ eVote360_Pro.WebApp/Models/UserIndexViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using eVote360_Pro.Core.Application.DTOs.User;
+
+namespace eVote360_Pro.WebApp.Models
+{
+    public class UserIndexViewModel
+    {
+        public List<UserGetDto> Users { get; set; } = new();
+        public bool HasActiveElection { get; set; }
+    }
+}
