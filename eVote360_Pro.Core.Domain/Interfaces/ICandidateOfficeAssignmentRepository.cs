68226002445d6b26475dfb69ade8c2628c240561
Gabriel Alcequiez <gxbbab@gmail.com>
2026-06-15 22:14:59 -0400

    feat(repository): add CandidateOfficeAssignmentRepository implementation

---
+++ eVote360_Pro.Core.Domain/Interfaces/ICandidateOfficeAssignmentRepository.cs
@@ -0,0 +1,11 @@
+using eVote360_Pro.Core.Domain.Entities;
+
+namespace eVote360_Pro.Core.Domain.Interfaces
+{
+    public interface ICandidateOfficeAssignmentRepository : IBaseRepository<CandidateOfficeAssignment>
+    {
+        Task<bool> CandidateHasOfficeAssigned(Guid candidateId, Guid partyId);
+        Task<bool> OfficeHasCandidateAssigned(Guid officeId, Guid partyId);
+        Task<CandidateOfficeAssignment?> GetAssignmentByCandidateAndParty(Guid candidateId, Guid partyId);
+    }
+}
---
+++ eVote360_Pro.Persistence/Repositories/CandidateOfficeAssignmentRepository.cs
@@ -0,0 +1,28 @@
+using eVote360_Pro.Core.Domain.Entities;
+using eVote360_Pro.Core.Domain.Interfaces;
+using eVote360_Pro.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace eVote360_Pro.Persistence.Repositories
+{
+    public class CandidateOfficeAssignmentRepository(AppDbContext context) : BaseRepository<CandidateOfficeAssignment>(context), ICandidateOfficeAssignmentRepository
+    {
+        public async Task<bool> CandidateHasOfficeAssigned(Guid candidateId, Guid partyId)
+        {
+            return await _context.CandidateOfficeAssignments
+                .AnyAsync(x => x.CandidateId == candidateId && x.PoliticalPartyId == partyId);
+        }
+
+        public async Task<bool> OfficeHasCandidateAssigned(Guid officeId, Guid partyId)
+        {
+            return await _context.CandidateOfficeAssignments
+                .AnyAsync(x => x.ElectedOfficeId == officeId && x.PoliticalPartyId == partyId);
+        }
+
+        public async Task<CandidateOfficeAssignment?> GetAssignmentByCandidateAndParty(Guid candidateId, Guid partyId)
+        {
+            return await _context.CandidateOfficeAssignments
+                .FirstOrDefaultAsync(x => x.CandidateId == candidateId && x.PoliticalPartyId == partyId);
+        }        
+    }
+}
